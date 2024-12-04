3e152be902ac3133da9bd0141aea8cee6f75f5aa
ajithms-01 <ajith7.official@gmail.com>
2024-12-04 12:12:08 +0530

    Update department_ms_enum.sql

--- department_ms_enum.sql
+++ department_ms_enum.sql
@@ -15,7 +15,7 @@ CREATE TABLE `department` (
   `dept_id` INT AUTO_INCREMENT NOT NULL,
   `dept_name` VARCHAR(100) NOT NULL,
   `location` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY (`dept_id`)
+  PRIMARY KEY (`dept_id`,`dept_name`)
 );
 
 -- Table 2: Employee
